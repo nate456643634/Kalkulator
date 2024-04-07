e954f88cdf0e84e687d45556d6ea9c06b31bd58a
nate456643634 <70407472+nate456643634@users.noreply.github.com>
2024-04-07 22:46:52 +0300

    Add files via upload

--- Goland/main.go
+++ Goland/main.go
@@ -18,7 +18,7 @@ var romeToArabic = map[string]int{
 	"IX":   9,
 	"X":    10,
 }
-
+// ПРИМИЧАНИЕ, Я СДЕЛАЛ ЛИШЬ ПОЛОВИНУ ЗАДАНИЕ НО НЕ СМОГ СДЕЛАТЬ ОДИН ДЮЙМ ОТ ЭТОГО ЗАДАНИЕ, ТАК КАК Я НЕ СМОГ РЕШИТЬ КАК СДЕЛАТЬ ОТВЕТ В РИМСКИХ ЧИСЛАХ И ДЛЯ ЭТОГО Я ДУМАЮ МНЕ НУЖНО ЕЩЁ ВРЕМЕНИ НО ВРЕМЕНИ НЕТУ, УВЫ ТАК ЧТО ПРИСЫЛАЮ КАК ЕСТЬ, В ЛЮБОМ СЛУЧАЕ СПАСИБО ЗА ТЗ И ЗА ПОНИМАНИЕ!
 func main() {
 	fmt.Println("ДОБРО ПОЖАЛОВАТЬ В КАЛЬКУЛЯТОР ДВОЙНЫХ СТАНДАРТОВ И ДВОЙНЫХ ЧИСЕЛ ВЫ ЗДЕСЬ МОЖЕТЕ ПРОВОДИТЬ КАЛЬКУЛИТИВНЫЕ ОПЕРАЦИИ НА АРАБСКИХ И РИМСКИХ ЧИСЕЛ")
 	fmt.Println("ЧТОБЫ ИСПОЛЬЗОВАТЬ КАЛЬКУЛЯТОР ПРОСТО НАПИШИТЕ ОПЕРАЦИЮ ОДНОЙ СТРОКОЙ")
@@ -26,54 +26,88 @@ func main() {
 		var num1Str string
 		var operate string
 		var num2Str string
+		
 
 		var num1 int
 		var num2 int
 
 		fmt.Scan(&num1Str, &operate, &num2Str)
-
 		num1, _ = strconv.Atoi(num1Str)
 		num2, _ = strconv.Atoi(num2Str)
-
+		
+        
 		if IsRomanNumbers(num1Str) {
 			num1 = romeToArabic[strings.TrimSpace(num1Str)]
 			num2 = romeToArabic[strings.TrimSpace(num2Str)]
 
 			switch operate {
 			case "+":
-				sum := num1 + num2
-				fmt.Println(sum)
+				result := num1 + num2
+				if result <= 10 {
+					fmt.Println(result)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "-":
-				sum := num1 - num2
-				fmt.Println(sum)
+				result := num1 - num2
+				if result <= 10 {
+					fmt.Println(result)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "*":
-				sum := num1 * num2
-				fmt.Println(sum)
+				result := num1 * num2
+				if result <= 10 {
+					fmt.Println(result)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "/":
-				sum := num1 / num2
-				fmt.Println(sum)
+				result := num1 / num2
+				if result <= 10 {
+					fmt.Println(result)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			default:
-				panic("АХТУНГ!! ВЫ ВВЕЛИ НЕ ТОТ СИМВОЛ ИЛИ ЦИФРУ")
+				panic("АХТУНГ!! ВЫ ВВЕЛИ НЕ ТОТ СИМВОЛ ИЛИ ЦИФРУ ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
 			}
 		} else {
 			switch operate {
 			case "+":
 				sum := num1 + num2
-				fmt.Println(sum)
+				if sum <= 10 {
+					fmt.Println(sum)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "-":
 				sum := num1 - num2
-				fmt.Println(sum)
+				if sum <= 10 {
+					fmt.Println(sum)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "*":
 				sum := num1 * num2
-				fmt.Println(sum)
+				if sum <= 10 {
+					fmt.Println(sum)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!! ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
+				}
 			case "/":
 				sum := num1 / num2
-				fmt.Println(sum)
+				if sum <= 10 {
+					fmt.Println(sum)
+				} else {
+					panic("АХТУНГ!!! ОТВЕТ НЕ ДОЛЖЕН БЫТЬ БОЛЬШЕ ДЕСЯТИ!!!!ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ ")
+				}
 			default:
-				panic("АХТУНГ!! ВЫ ВВЕЛИ НЕ ТОТ СИМВОЛ ИЛИ ЦИФРУ")
+				panic("АХТУНГ!! ВЫ ВВЕЛИ НЕ ТОТ СИМВОЛ ИЛИ ЦИФРУ ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ")
 			}
 		}
 	}
+
 }
 
 func IsRomanNumbers(s string) bool {
